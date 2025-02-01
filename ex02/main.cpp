b0c1f33d2816d41a563a1185c07c03b9942b0de6
tomoya baba <tomoya.bb17931837@gmail.com>
2025-02-01 22:04:57 +0900

    ex02 done

---
+++ ex02/main.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPRT = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "The memory address of the string variable:	"<< &string << '\n';
+	std::cout << "The memory address held by stringPTR:		" << stringPRT << '\n';
+	std::cout << "The memory address held by stringREF:		" << &stringREF << '\n';
+	std::cout << "The value of the string variable:	" << string << '\n';
+	std::cout << "The value pointed to by stringPTR:	" << *stringPRT << '\n';
+	std::cout << "The value pointed to by stringREF:	" << stringREF << '\n';
+
+	// stringREF = "HI THIS IS MUSCLE";
+	// std::cout << "The memory address of the string variable:	"<< &string << '\n';
+	// std::cout << "The memory address held by stringPTR:		" << stringPRT << '\n';
+	// std::cout << "The memory address held by stringREF:		" << &stringREF << '\n';
+	// std::cout << "The value of the string variable:	" << string << '\n';
+	// std::cout << "The value pointed to by stringPTR:	" << *stringPRT << '\n';
+	// std::cout << "The value pointed to by stringREF:	" << stringREF << '\n';
+	return (0);
+}

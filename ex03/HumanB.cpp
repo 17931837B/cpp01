3676adf4238f66ffabef153ff5893763ce3f7d51
tomoya baba <tomoya.bb17931837@gmail.com>
2025-02-02 17:27:30 +0900

    ex04 done

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,9 +6,9 @@ HumanB::HumanB(std::string name) : name_p(name), weapon_p(NULL)
 void HumanB::attack()
 {
 	if (weapon_p)
-		std::cout << name_p << " attacks with their " << (*weapon_p).getType() << std::endl;
+		std::cout << name_p << " attacks with their " << (*weapon_p).getType() << '\n';
 	else
-		std::cout << name_p << " attacks with their " << "heart" << std::endl;
+		std::cout << name_p << " attacks with their " << "heart" << '\n';
 }
 
 void	HumanB::setWeapon(Weapon &weapon)
---
+++ ex04/main.cpp
@@ -0,0 +1,61 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+std::string	ft_replace(std::string line, const std::string& from, const std::string& to)
+{
+	size_t	pos;
+	
+	pos = line.find(from);
+	while (pos != std::string::npos)
+	{
+		line.replace(pos, from.length(), to);
+		pos = line.find(from, pos + to.length());
+	}
+	return (line);
+}
+
+int	main(int argc, char **argv)
+{
+	std::string	line;
+	std::string	rep;
+
+	if (argc != 4)
+	{
+		std::cout << "Argument Error\n";
+		return (1);
+	}
+	std::ifstream fin(argv[1]);
+	if (!fin)
+	{
+		std::cout << "Can't open file\n";
+		return (1);
+	}
+	rep = ".replace";
+	std::ofstream fout(argv[1] + rep);
+	if (!fout)
+	{
+		std::cout << "Error open file\n";
+		return (1);
+	}
+	while (1)
+	{
+		if (std::getline(fin, line))
+		{
+			line = ft_replace(line, argv[2], argv[3]);
+			fout << line << '\n';
+			if (fin.eof())
+				break ;
+		}
+		else
+		{
+			std::cout << "Error reading\n";
+			fout.close();
+			fin.close();
+			return (1);
+		}
+	}
+	fout.close();
+	fin.close();
+	return (0);
+}
